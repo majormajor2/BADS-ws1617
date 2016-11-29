a422fd92c64881c2bbbcfc53cf2b826a47a32c62
dkm1006 <aurekomp@gmx.de>
2016-11-29 17:52:20 +0100

    Update helper.R

--- helper.R
+++ helper.R
@@ -3,11 +3,11 @@
 get_dataset = function(name) {
   data = read.csv(name, header=T,sep=";")
   
-  data$title = factor(data$title, labels=c("no", "yes"))
-  data$newsletter = factor(data$newsletter, labels=c("no", "yes"))
-  data$coupon = factor(data$coupon, labels=c("no", "yes"))
-  data$giftwrapping = factor(data$giftwrapping, labels=c("no", "yes"))
-  data$referrer = factor(data$referrer, labels=c("no", "yes"))
+
+  for(header in list(data$title,data$newsletter,data$coupon,data$giftwrapping,data$referrer,data$cost_shipping)
+      {
+      header = factor(header, labels=c("no,"yes"))
+      }
 
   #data$YOB[data$YOB==99] = NA
   #data$YOB_missing = factor(ifelse(is.na(data$YOB), 1, 0), labels=c("no","yes"))
