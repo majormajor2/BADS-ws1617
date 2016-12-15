ca2a26892bb47f00dd876729defdbb1847f2a73d
orenbl <orenbl@gmail.com>
2016-12-15 17:45:24 +0100

    Created a matrix of outliers. Need to think how to plot them all in a convenient way and how to decide which variables to truncate.

--- Outliecheck.R
+++ Outliecheck.R
@@ -1,5 +1,5 @@
 # set to your library
-setwd("/Users/orenblumenfeld/Google Drive/MEMS Degree/WiSe16:17/Business Analytics/Assignment")
+setwd("/Users/orenblumenfeld/Google Drive/MEMS Degree/WiSe16:17/Business Analytics/Assignment/bads-ws1617-group27")
 # read the data set and summarize it for fun
 known <- read.csv("assignment_BADS_WS1617_known.csv", sep = ",", header = TRUE)
 str(known)
@@ -9,7 +9,9 @@ known.outlierscheck <- known
 # nullify uneeded columns
 known.outlierscheck[ ,c(1:14,17:22)] <- c(NULL)
 # download and install matrixStats package.
-install.packages("matrixStats")
-library(matrixStats)
+if(!require("matrixStat")) install.packages("matrixStats"); library(matrixStats)
 # Check probabilities for the relevant columns.
-colQuantiles(known.outlierscheck, probs=seq(from = 0, to = 1, by = 0.25), na.rm = TRUE)
+known.outlierscheck.names <- colnames(known.outlierscheck)
+# create matrix of the quantiles
+known.quantiles <- matrix(data = colQuantiles(known.outlierscheck, probs=seq(from = 0, to = 1, by = 0.25), na.rm = TRUE), nrow = 5, ncol = ncol(known.outlierscheck), byrow = TRUE)
+colnames(known.quantiles) <- known.outlierscheck.names
