2465a4b68220b4ddcbd63580485b6120168c9b52
dkm1006 <aurekomp@gmx.de>
2016-12-16 19:15:37 +0100

    Updated helper function get_dataset

--- helper.R
+++ helper.R
@@ -1,13 +1,41 @@
-# name = "~/Assignment_BADS_WS1617/assignment_BADS_WS1617_known.csv"
+
+# getting the dataset, creating a data.frame and 
+# converting variables to correct data types
+# input: filename of csv-file
+# output: data frame
 
 get_dataset = function(name) {
-  data = read.csv(name, header=T,sep=";")
+  # read csv-file
+  data = read.csv(name, header=T,sep=",")
   
-
-  for(header in list(data$title,data$newsletter,data$coupon,data$giftwrapping,data$referrer,data$cost_shipping))
+  
+  # drop data$points_redeemed because it has all zeros -> no informational value
+  data$points_redeemed = NULL
+  
+  # factorise ID
+  data$ID = factor(data$ID)
+  
+  # factorise postcodes
+  data$postcode_invoice = factor(data$postcode_invoice)
+  data$postcode_delivery = factor(data$postcode_delivery)
+  
+  # factorise website model
+  data$model = factor(data$model)
+  
+  # factorise all binary variables with labels "no" and "yes" where appropriate
+  for(header in c("title","newsletter","coupon","giftwrapping","referrer","cost_shipping"))
       {
-      header = factor(header, labels=c("no","yes"))
-      }
+      data[,header] = factor(data[,header],labels=c("no","yes"))
+  }
+  
+  # factorise binary variable data$delivery with appropriate labels
+  data$delivery = factor(data$delivery, labels=c("Door delivery","Collection at post office"))
+  
+  for(header in colnames(data))
+    {
+    print(header)
+    summary(data[,header])
+    }
 
   #data$YOB[data$YOB==99] = NA
   #data$YOB_missing = factor(ifelse(is.na(data$YOB), 1, 0), labels=c("no","yes"))
