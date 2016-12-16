481cdc140668b52d7eaa79a335be9d5f1b737165
dkm1006 <aurekomp@gmx.de>
2016-12-16 21:20:08 +0100

    Updated main.R

--- main.R
+++ main.R
@@ -1,4 +1,6 @@
 # This is the main script that runs all the other modules
 
 source("helper.R")
-get_dataset("assignment_BADS_WS1617_class.csv")
+known = get_dataset("assignment_BADS_WS1617_class.csv")
+lapply(known,class)
+lapply(known,summary)
