083355b054854e8204dfdf706d3680aa05ec59e0
dkm1006 <aurekomp@gmx.de>
2016-12-17 12:36:36 +0100

    Removed error in main.R

--- main.R
+++ main.R
@@ -1,6 +1,6 @@
 # This is the main script that runs all the other modules
 
 source("helper.R")
-known = get_dataset("assignment_BADS_WS1617_class.csv")
+known = get_dataset("assignment_BADS_WS1617_known.csv")
 lapply(known,class)
 lapply(known,summary)
