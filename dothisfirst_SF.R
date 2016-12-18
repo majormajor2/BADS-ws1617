210836471188fad5bbb5e71366bc2820b0d66cf2
ste-phanie <finkenws@hu-berlin.de>
2016-12-18 12:39:10 +0100

    Get Data and load packages for Steffi

---
+++ dothisfirst_SF.R
@@ -0,0 +1,15 @@
+
+#get data
+setwd("/Users/sfinkenwirth/Documents/MEMS/Lectures and Seminars/Business Analytics & Data Science/Assignment_BADS_WS1617/Input")
+known <- read.csv("assignment_BADS_WS1617_known.csv", header = TRUE, sep = ",")
+
+# load packages
+if(!require("ggplot2")) install.packages("ggplot2"); library(ggplot2)
+
+
+# fix plot resolutions
+par(mar=c(1,1,1,1)) # this code avoids some errors related to screen resolution; feel free to ignore it
+
+# structure of dataset
+str(known)
+summary(known)
