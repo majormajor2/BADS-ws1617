a7a3919a5b3e95bdafed35964f97e818abf1e771
Hamayun Khan <Hamayun Khan>
2016-12-21 10:21:30 +0100

    Minor changes - saved 12.21

--- Predictivemodelling.R
+++ Predictivemodelling.R
@@ -1,5 +1,5 @@
 
-#1.  Load dataset using the helper function
+#1. Load dataset using the helper function
 #2. Use the data cleaning (dates) function 
 #3. Partition data into train, test and validation data sets as we did in the tutorial
 #4. Try to develop the models used in TUT 4 onwards, starting with Logistic, 
@@ -7,6 +7,7 @@
 #5. Model evaluation (looking only at DT and LR)
 #6. Try K-folds validtion - TUT 6
 #7. Neural Networks
+
 
 
 #1.  Load dataset using the helper function
@@ -168,6 +169,8 @@ for (i in 1:k) {
   # Build and evaluate models using these partitions
   lr <- glm(return_customer ~ goods_value + item_count + account_creation_date_missing + deliverydate_actual_missing, data = cv.train, family = binomial(link = "logit"))
   dt <- rpart(return_customer ~ goods_value + item_count + account_creation_date_missing + deliverydate_actual_missing, data = cv.train, cp = 0.015) # create decision tree classifier
+  # LR doesn't seem to work when selecting all columns
+  
   yhat.lr <- predict(lr, newdata = cv.val, type = "response")
   yhat.dt <- predict(dt, newdata = cv.val, type = "prob")[,2]
   # We use our above function to calculate the classification error
@@ -189,4 +192,71 @@ yhat.test.dt.prunedMore <- predict(dt, newdata = test, type = "prob")[,2]
 brier.test <- sapply(list("lr" = yhat.test.lr, "dt" = yhat.test.dt.prunedMore), BrierScore, y = y.test, USE.NAMES = TRUE)
 print(brier.test)
 
-#7. 
+#7. Neural Network
+if(!require("nnet")) install.packages("nnet"); library("nnet") # Try to load rpart, if it doesn't exist, then install and load it
+if(!require("pROC")) install.packages("pROC"); library("pROC") # Try to load rpart, if it doesn't exist, then install and load it
+if(!require("caret")) install.packages("caret"); library("caret") 
+
+  ### Specify the setup:
+  # The number of nodes to try for the model
+nnet.sizes <- seq(from = 3, to = 15, by = 3)
+  # Initialize the data frame that collects the results
+results <- as.data.frame(matrix(NA, ncol = length(nnet.sizes), nrow = k))
+for(n in 1:length(nnet.sizes)){
+  # This is the cross-validation loop from before
+  for (i in 1:k) {
+    # Split data into training and validation
+    idx.val <- which(folds == i, arr.ind = TRUE)
+    cv.train <- train.rnd[-idx.val,]
+    cv.val <- train.rnd[idx.val,]
+    # Train the neural network model with a number of nodes n
+    #!!!model doesn't run on the entire data set :(
+    neuralnet <- nnet(return_customer~order_date + form_of_address + title + newsletter + model + goods_value + item_count + canceled_items, data = cv.train, # the data and formula to be used
+                      trace = FALSE, maxit = 1000, # general options
+                      size = nnet.sizes[n]) # the number of nodes in the model
+    # Build and evaluate models using these partitions
+    yhat <- predict(neuralnet, newdata = cv.val, type = "raw")
+    # Calculate the AUC value with a function from package pROC
+    results[i, n] <- auc(cv.val$return_customer, as.vector(yhat))
+  }
+}
+
+colnames(results) <- nnet.sizes 
+boxplot(results, xlab = "Number of nodes in hidden layer", ylab = "Area under the ROC curve (AUC)")
+
+model.control<- trainControl(
+  method = "cv", # 'cv' for cross validation
+  number = 5, # number of folds in cross validation
+  #repeats = 3, # number for repeated cross validation
+  classProbs = TRUE,
+  summaryFunction = twoClassSummary,
+  allowParallel = TRUE, # Enable parallelization if available
+  returnData = FALSE # The training data will not be included in the ouput training object
+)
+
+train$return_customer <- factor(train$return_customer, labels = c("yes", "no"))
+
+nn.parms <- expand.grid(decay = c(0, 10^seq(-3, 0, 1)), size = seq(3,15,2))
+
+nn <- train(return_customer~order_date + newsletter + model + goods_value + item_count + canceled_items, data = train,  
+            method = "nnet", maxit = 200, trace = FALSE, # options for nnet function
+            tuneGrid = nn.parms, # parameters to be tested
+            metric = "ROC", trControl = model.control)
+
+print(nn)
+plot(nn)
+summary(nn)
+
+
+yhat.nn   <- predict(nn, newdata = test, type = "prob")[,2]
+nn.roc <-roc(test$return_customer, yhat.nn)
+auc(nn.roc)
+plot.roc(nn.roc)
+
+h <- HMeasure(true.class = as.numeric(test$return_customer)-1, scores = yhat.nn)
+# Note that AUC is the same; as it should be
+h$metrics["AUC"]
+plotROC(h, which=1)
+
+known$return_customer <- as.numeric(known$return_customer) -1
+
