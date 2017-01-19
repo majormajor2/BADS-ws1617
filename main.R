1c1682b827098a50a6714091e61a1b370238ed9d
dkm1006 <aurekomp@gmx.de>
2017-01-19 21:34:06 +0100

    Took out adaptive boosting from main.R so it can be used as a "run first, then experiment" kind of script

--- main.R
+++ main.R
@@ -87,35 +87,3 @@ test_data  =  known[-idx_train, ] # test set (drop all observations with train i
 # train_data = train_data[-idx_validation, ]
 
 
-
-######## Try adaptive boosting ###############
-
-adaboost = adaptive_boosting(train_data[,grep("weight",train_data,invert = TRUE)])
-summary(adaboost)
-adaboost$trees
-adaboost$weights
-
-errorevol(adaboost,train_data[,grep("weight",train_data,invert = TRUE)])
-adaboost_prediction = predict(adaboost,test_data[,grep("weight",test_data,invert = TRUE)])
-
-# list the variables by their importance
-adaboost_importance = sort(adaboost$importance, decreasing = TRUE)
-adaboost_importance[adaboost_importance > 0]
-
-# print a variable importance plot
-print_importance_plot(adaboost_importance)
-
-# plot a decision tree
-adaboost_tree = adaboost$trees[[1]]
-
-#plot(adaboost_tree)
-#text(adaboost_tree,pretty=0)
-
-
-######## Check predictive performance ###############
-
-#confusionMatrix(data = prediction, reference = known$return_customer, positive = "yes")
-predictive_performance(y = test_data$return_customer, prediction = adaboost_prediction$prob[,2])
-
-
-#######################
