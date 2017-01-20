76245acc177d197284932587f85ff998a22730c8
ste-phanie <finkenws@hu-berlin.de>
2017-01-20 11:41:37 +0100

    - produces correlation matrix on numeric variables - outputs pdf

--- correlation matrix.R
+++ correlation matrix.R
@@ -1,4 +1,4 @@
-### DATA CORRELATIONS ###
+### DATA CORRELATIONS (tutorial 3) ###
 
 # Calculate the correlation matrix
 # We select the numeric variabels inside of the function by applying function is.numeric to each element of dataframe loans (i.e. each variable)
@@ -7,9 +7,13 @@ cmatrix <- cor(known[, idx_numeric])
 # Look at the correlation matrix
 print(cmatrix)
 # Install the package "corrplot"
-install.packages("corrplot")
-library(corrplot)
+if(!require("corrplot")) install.packages("corrplot");library(corrplot)
 # Create a plot of your correlation matrix 
 corrplot(cmatrix)
 # The size of the dots indicates how strong the variables correlate with each other, while the color shows direction
-```
+
+# export everything to pdf
+dev.off()
+pdf(file = paste("correlation matrix.pdf"))
+corrplot(cmatrix)
+dev.off()
