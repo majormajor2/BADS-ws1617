68f7cc7648b7834e7192ae2b2f6735685a262672
dkm1006 <aurekomp@gmx.de>
2017-01-27 16:05:56 +0100

    Added deep learning network package

--- later_stage.R
+++ later_stage.R
@@ -1,16 +1,36 @@
+######## Standardize  ###############
+multilevel_factors = c("form_of_address", "email_domain", "model", "payment", "postcode_invoice", "postcode_delivery", "advertising_code")
+print("Performing normalization on all parameters that are not multilevel factors:")
+for(column in colnames(known))
+{
+  if(!column %in% multilevel_factors)
+  {
+    print(column)
+    known[column] = sapply(known[column],as.numeric)
+    known[column] = normalize_cardinal_variables(known[column])
+  }
+}
+#known[columns_to_replace] = sapply(known[-columns_to_replace], as.numeric)
+#known[columns_to_replace] = sapply(known[-columns_to_replace], normalize_cardinal_variables)
+
+idx_train  = createDataPartition(y = known$return_customer, p = 0.5, list = FALSE) 
+train_data = known[idx_train, ] # training set
+test_data  =  known[-idx_train, ] # test set (drop all observations with train indices)
+
 ######## Build models ###############
 
 # starting with Logistic, dt and then neural networks
 # set the number of nodes and hidden layers for the neural net
-number_of_nodes = 6
+number_of_nodes = 10
 number_of_layers = 1
 
 linear_model = glm(return_customer ~ ., data = train_data, family = binomial(link = "logit"))
 decision_tree = rpart(return_customer ~ ., data = train_data, method = "class", cp = 0.0001)
 neuralnet = nnet(return_customer~ ., data = train_data, # the data and formula to be used
                   trace = FALSE, maxit = 1000, # general options
-                  size = 20, # the number of nodes in the model
+                  size = number_of_nodes, # the number of nodes in the model
                   MaxNWts = 10000) 
+deep_arch = darch(return_customer~ ., data = train_data, bp.learnRate = 0.5)
 
 #Helpful functions in reading rpart output
 printcp(decision_tree)
