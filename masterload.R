e0b21eeec0033033862c9703338b125c2ed0eff1
ste-phanie <finkenws@hu-berlin.de>
2017-02-01 12:18:26 +0100

    file to load master and file to save master

---
+++ masterload.R
@@ -0,0 +1,4 @@
+#### LOAD Master
+
+# loads current version of master file from github
+df_predictions_test <- call_master(filename.csv = "predictions_test.csv")
---
+++ mastersave.R
@@ -0,0 +1,4 @@
+### SAVE Master
+
+# this saves the master to csv (needs to be pushed!)
+df_predictions_test <- save_prediction_to_master(filename.csv = "predictions_test.csv", master = df_predictions_test)
