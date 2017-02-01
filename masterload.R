b86ebe9f32ac49a39ddf8aef0c89b9bf2ce05690
ste-phanie <finkenws@hu-berlin.de>
2017-02-01 14:51:58 +0100

    ads code for validation dataset

--- masterload.R
+++ masterload.R
@@ -2,3 +2,4 @@
 
 # loads current version of master file from github
 df_predictions_test <- call_master(filename.csv = "predictions_test.csv")
+df_predictions_validation <- call_master(filename.csv = "predictions_validation.csv")
