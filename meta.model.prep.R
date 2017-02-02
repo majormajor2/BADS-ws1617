5c47593fb3e197d76f4a6679de7c544429aae7cf
Majormajor2 <jahaz707@gmail.com>
2017-02-02 11:10:14 +0100

    Saving changes to meta.model and df_test

--- meta.model.prep.R
+++ meta.model.prep.R
@@ -161,8 +161,10 @@ xgb_pca_default_score <-predictive_performance(test_data_woe$return_customer, xg
 
 ### 5. SAVE PREDICTIONS IN DF
 
+df_predictions_test = data.frame(return_customer = test_data$return_customer)
+
 #Call validation file to save predictions
-df_predictions_test = call_master("predictions_validation.csv")
+df_predictions_test = call_master("predictions_test.csv")
 
 #Add predictions
 df_predictions_test$xgb.default.pred = xgb.default.pred #hamayun
@@ -172,10 +174,83 @@ df_predictions_test$xgb_woe.default.pred = xgb_woe.default.pred #hamayun
 df_predictions_test$xgb_woe.param1.pred = xgb_woe.param1.pred #oren
 df_predictions_test$xgb_woe.param2.pred = xgb_woe.param2.pred #oren
 df_predictions_test$xgb_woe_ef.default = xgb_woe_ef.default #hamayun
-df_predictions_testn$xgb_woe_ew.default = xgb_woe_ew.default #hamayun
+df_predictions_test$xgb_woe_ew.default = xgb_woe_ew.default #hamayun
 df_predictions_test$xgb.pca.pred = xgb.pca.pred #hamayun
 
 #Save validation file
-df_predictions_test = save_prediction_to_master("predictions_validation.csv", df_predictions_validation)
+df_predictions_test = save_prediction_to_master("predictions_test.csv", df_predictions_test)
 #Remember to push afterwards
+
+View(df_predictions_test)
+
+
+### 6. META META
+# 6.1 Logistic regression
+lr <- glm(return_customer ~., data = df_predictions_validation_1, family = binomial(link="logit"))
+meta.lr <- predict(lr, newdata = df_predictions_test, type = "response")
+predictive_performance(test_data$return_customer, pred.lr, cutoff = 0.201)
+
+# 6.2 Random forest
+
+k <- 5
+# Set a seed for the pseudo-random number generator
+set.seed(123)
+model.control <- trainControl(
+  method = "cv", # 'cv' for cross validation
+  number = k, # number of folds in cross validation
+  classProbs = TRUE, # Return class probabilities
+  summaryFunction = twoClassSummary, # twoClassSummary returns AUC
+  allowParallel = TRUE # Enable parallelization if available
+)
+
+rf.parms <- expand.grid(mtry = 1:10)
+
+rf.meta <- train(return_customer~., 
+                 data = df_predictions_validation_1,  
+                 method = "rf", 
+                 ntree = 500, 
+                 tuneGrid = rf.parms, 
+                 metric = "ROC", 
+                 trControl = model.control)
+#Predict
+meta.rf   <- predict(rf.meta, newdata = df_predictions_test, type = "prob")[,2]
+
+#xgb meta
+## the options for model selection
+model.control<- trainControl(
+  method = "cv", # 'cv' for cross validation
+  number = 5, # number of folds in cross validation
+  classProbs = TRUE,
+  summaryFunction = twoClassSummary,
+  allowParallel = TRUE, # Enable parallelization if available
+  returnData = TRUE # We will use this to plot partial dependence
+)
+
+
+## Define a search grid for model selection
+xgb.parms.default <- expand.grid(nrounds = c(20, 40, 60, 80), 
+                                 max_depth = c(2, 4), 
+                                 eta = c(0.01, 0.05, 0.1, 0.15), 
+                                 gamma = 0,
+                                 colsample_bytree = c(0.8, 1),
+                                 min_child_weight = 1,
+                                 subsample = 0.8)
+# Model
+xgb.default <- train(return_customer~., data = df_predictions_validation_1,  
+                     method = "xgbTree",
+                     tuneGrid = xgb.parms.default,
+                     metric = "ROC", 
+                     trControl = model.control)
+
+meta.xgb = xgb.default
+
+#Predict using XGB
+xgb.meta <- predict(xgb.default, newdata = df_predictions_test_1, type = "prob")[,2]
+predictive_performance(test_data$return_customer, xgb.meta, cutoff = 0.342)
+
+## Saving to a csv
+df_predictions_test = call_master("df_predictions_test.csv")
+df_predictions_test$meta.rf = meta.xgb #getting an error, need to fix it#
+df_predictions_test = save_prediction_to_master("df_predictions_test.csv", df_predictions_test)
+
 
