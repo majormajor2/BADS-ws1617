5e8e8e117c78eb3ca4778a978f00308accd4b3a6
ste-phanie <finkenws@hu-berlin.de>
2017-02-02 01:24:57 +0100

    fixed mistake

--- masterload.R
+++ masterload.R
@@ -6,5 +6,5 @@ df_predictions_validation <- call_master(filename.csv = "predictions_validation.
 df_performance_validation <- call_master(filename.csv = "performance_validation.csv")
 # test
 df_predictions_test <- call_master(filename.csv = "predictions_test.csv")
-df_performance_test <- call_master(filename.csv = "predictions_test.csv")
+df_performance_test <- call_master(filename.csv = "performance_test.csv")
 
