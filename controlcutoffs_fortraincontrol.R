08ed95e41acba2d0cf8885014b03c07ac43c1e49
ste-phanie <finkenws@hu-berlin.de>
2017-02-04 22:02:20 +0100

    test for traincontrol

---
+++ controlcutoffs_fortraincontrol.R
@@ -0,0 +1,52 @@
+### Custom function for train.control
+
+# use avg retrun as metric to choose the model
+# output: average return
+
+### Optimal Cutpoints
+
+if(!require("OptimalCutpoints")) install.packages("OptimalCutpoints"); library("OptimalCutpoints")intsource("performance_measures.R")
+
+
+###### --------- control.cutpoints ---------  ######
+
+# input: dataframe with predictions
+
+optimalCutpoint <- function(data, lev = NULL, model = NULL){
+  
+  # GET COST MATRIX
+  cost.matrix <- build_cost_matrix()
+  
+  # MODEL.CONTROL
+  # method: maxKappa
+  model.control.optc = control.cutpoints(CFP = -cost.matrix[2,1], CFN = -cost.matrix[1,2], costs.ratio = -cost.matrix[2,1]/-cost.matrix[1,2], weighted.Kappa = TRUE)
+
+    # RUN OPTIMAL CUTPOINTS
+    oc = optimal.cutpoints(
+      X = "yes", 
+      status = "obs", 
+      tag.healthy = "no", 
+      methods = "MCT", 
+      data = data, 
+      control = model.control.optc)
+    
+    # SELECT OPTIMAL CUTPOINT & store hmeasure and AUC in dataframe
+    # define temporary dataframes to store cutoffs 
+    df <- data.frame(cutoff = oc$MCT$Global$optimal.cutoff$cutoff)
+    # check if cutpoint unique                  
+    for(index in 1:length(oc$MCT$Global$optimal.cutoff$cutoff)){
+      # optimal cutpoint
+      df[index,"avg_return"] <- predictive_performance(dataframe_pred$return_customer, prediction = dataframe_pred[,pred], cutoff = df[index,"cutoff"], returnH = FALSE)$avg_return
+    }
+    opt.cutoff <- df[df$avg_return == max(df$avg_return), "cutoff"]
+    avg_return <- predictive_performance(y = data$obs, prediction = data$yes, cutoff = opt.cutoff, returnH = FALSE)$avg_return
+    
+    
+   
+  # OUTPUT
+  return(avg_return)
+}
+
+
+
+
---
+++ testrun_advancedstuff.R
@@ -0,0 +1,26 @@
+### 1. SETUP
+## the options for model selection
+model.control<- trainControl(
+  method = "cv", # 'cv' for cross validation
+  number = 5, # number of folds in cross validation
+  classProbs = TRUE,
+  summaryFunction = optimalCutpoint,
+  allowParallel = TRUE, # Enable parallelization if available
+  returnData = TRUE # We will use this to plot partial dependence
+)
+## TUNES
+xgb.parms.default <- expand.grid(nrounds = c(20, 40, 60, 80), 
+                                 max_depth = c(2, 4), 
+                                 eta = c(0.01, 0.05, 0.1, 0.15), 
+                                 gamma = 0,
+                                 colsample_bytree = c(0.8, 1),
+                                 min_child_weight = 1,
+                                 subsample = 0.8)
+
+# RUN MODEL
+xgb_woe.default <- train(return_customer~., 
+                         data = train_data_woe,  
+                         method = "xgbTree",
+                         tuneGrid = xgb.parms.default,
+                         metric = "avg_return", 
+                         trControl = model.control)
