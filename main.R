e32007f87998955a8b003bfb8a6307bb171f1410
dkm1006 <aurekomp@gmx.de>
2017-02-05 19:11:18 +0100

    Updated main.R

--- main.R
+++ main.R
@@ -7,6 +7,8 @@ source("weight.R")
 source("woe.R")
 source("performance_measures.R")
 source("binning.R")
+source("controlcutoffs_fortraincontrol.R")
+source("predictclass.R")
 
 ####### Set seed ################
 
@@ -53,106 +55,39 @@ idx_train  = createDataPartition(y = known$return_customer, p = 0.8, list = FALS
 train_data = known[idx_train, ] # training set
 test_data  =  known[-idx_train, ] # test set (drop all observations with train indices)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-set.seed(999) # just making sure ;)
-idx_validation = createDataPartition(y = train_data$return_customer, p = 0.25, list = FALSE)
-train60_data = train_data[-idx_validation, ] # this is the smaller 60% dataset for training before validation
-validation_data = train_data[idx_validation, ] # Validation is for testing the models before the meta model is run
-
-###### Nested Cross Validation ######
-
-###### Weight of Evidence ######
+###### Calculate Weights of Evidence #########
 # Will create a new dataframe consisting of all the variables of known but replaces the factor
 # variables into numerical variables according to the weight of evidence
 columns_to_replace = c("form_of_address", "email_domain", "model", "payment", "postcode_invoice", "postcode_delivery", "advertising_code")
-# Calculate WoE from train_data and return woe object
-woe_object = calculate_woe(train60_data, target = "return_customer", columns_to_replace = columns_to_replace)
-# train 80
-woe_object_train = calculate_woe(train_data, target = "return_customer", columns_to_replace = columns_to_replace)
-# Replace multilevel factor columns in train_data by their woe
-train60_data_woe = apply_woe(dataset = train60_data, woe_object = woe_object)
-train_data_woe = apply_woe(dataset = train_data, woe_object = woe_object_train)
+# Calculate WoE from train_fold and return woe object
+woe_object = calculate_woe(known, target = "return_customer", columns_to_replace = columns_to_replace)
+# Replace multilevel factor columns in train_fold by their WoE
+known_woe = apply_woe(dataset = known, woe_object = woe_object)
+# Apply WoE to all the other folds 
+#validation_fold_woe = apply_woe(dataset = validation_fold, woe_object = woe_object)
+class_woe = apply_woe(dataset = class, woe_object = woe_object)
 
-# Apply woe to validation (input any dataset where levels are identical to trained woe_object)
-validation_data_woe = apply_woe(dataset = validation_data, woe_object = woe_object)
-# Apply woe to test (input any dataset where levels are identical to trained woe_object)
-test_data_woe = apply_woe(dataset = test_data, woe_object = woe_object_train)
+###### Normalize datasets ######
 
-# Calculate WoE for known data set
-woe_object_known = calculate_woe(known, target = "return_customer", columns_to_replace = columns_to_replace)
-known_woe = apply_woe(dataset = known, woe_object = woe_object_known)
+dropped_correlated_variables = strongly_correlated(known_woe, threshold = 0.6)
 
-# Apply woe to class (input any dataset where new levels emerge compared to training datset)
-class_woe = apply_woe(dataset = class, woe_object = woe_object_known)
-
-##### BINNING #######
-
-# creates bins for columns "form_of_address", "email_domain", "model", "payment", "postcode_invoice", "postcode_delivery", "advertising_code"
-# applies woe to binned columns
-
-# 1 CALCULATE WOE-OBJECT 
-# 1.1 create bins for train-dataset
-# train_data_bins
-train60_data_bins_ew = create_bins(train60_data_woe, NO_BINS = 5, DO_EQUAL_WIDTH = TRUE, run_woe = FALSE)
-train_data_bins_ew = create_bins(train_data_woe, NO_BINS = 5, DO_EQUAL_WIDTH = TRUE, run_woe = FALSE)
-train60_data_bins_ef = create_bins(train60_data_woe, NO_BINS = 5, DO_EQUAL_WIDTH = FALSE, run_woe = FALSE)
-train_data_bins_ef = create_bins(train_data_woe, NO_BINS = 5, DO_EQUAL_WIDTH = FALSE, run_woe = FALSE)
-
-# 1.2 calculate woe form binned train-datasets
-woe_object_ew = calculate_woe(train60_data_bins_ew, columns = c("email_domain", "postcode_invoice", "postcode_delivery", "advertising_code"))
-woe_object_ew_train80 = calculate_woe(train_data_bins_ew, columns = c("email_domain", "postcode_invoice", "postcode_delivery", "advertising_code"))
-woe_object_ef = calculate_woe(train60_data_bins_ef, columns = c("email_domain", "postcode_invoice", "postcode_delivery", "advertising_code"))
-woe_object_ef_train80 = calculate_woe(train_data_bins_ef, columns = c("email_domain", "postcode_invoice", "postcode_delivery", "advertising_code"))
-
-# train_data_woe
-train60_data_woe_ew = create_bins(train60_data_woe, woe_object_ew, NO_BINS = 5, DO_EQUAL_WIDTH = TRUE, run_woe = TRUE)
-train_data_woe_ew = create_bins(train_data_woe, woe_object_ew_train80, NO_BINS = 5, DO_EQUAL_WIDTH = TRUE, run_woe = TRUE)
-
-train60_data_woe_ef = create_bins(train60_data_woe, woe_object_ef, NO_BINS = 5, DO_EQUAL_WIDTH = FALSE, run_woe = TRUE)
-train_data_woe_ef = create_bins(train_data_woe, woe_object_ef_train80, NO_BINS = 5, DO_EQUAL_WIDTH = FALSE, run_woe = TRUE)
-
-# validation_data_woe
-validation_data_woe_ew = create_bins(validation_data_woe, woe_object_ew, NO_BINS = 5, DO_EQUAL_WIDTH = TRUE, run_woe = TRUE)
-validation_data_woe_ef = create_bins(validation_data_woe, woe_object_ef, NO_BINS = 5, DO_EQUAL_WIDTH = FALSE, run_woe = TRUE)
-
-# test_data_woe
-test_data_woe_ew = create_bins(test_data_woe, woe_object_ew_train80, NO_BINS = 5, DO_EQUAL_WIDTH = TRUE, run_woe = TRUE)
-test_data_woe_ef = create_bins(test_data_woe, woe_object_ef_train80, NO_BINS = 5, DO_EQUAL_WIDTH = FALSE, run_woe = TRUE)
-
-# class_data_woe
-class_woe_ew = create_bins(class_woe, woe_object_ew_train80, NO_BINS = 5, DO_EQUAL_WIDTH = TRUE, run_woe = TRUE)
-class_woe_ef = create_bins(class_woe, woe_object_ew_train80, NO_BINS = 5, DO_EQUAL_WIDTH = FALSE, run_woe = TRUE)
+print("Perform normalization operations.")
+known_norm = prepare(known_woe, dropped_correlated_variables)
+#validation_fold_woe = prepare(validation_fold_woe, dropped_correlated_variables)
+class_norm = prepare(class_woe, dropped_correlated_variables)
 
 
 ######## Cost matrix ##########
 cost.matrix = build_cost_matrix(CBTN = 3, CBFP = -10)
 
 
-####### Call Master File
+####### Call Master File ######
 df_predictions_test <- call_master(filename.csv = "predictions_test.csv")
 
 
-### Plotting
+#### Plotting ####
 par(mar=c(1,1,1,1)) # to make sure the plot works on a small screen
+
+
+##### Predict classification dataset #####
+predict_class()
--- predictclass.R
+++ predictclass.R
@@ -1,8 +1,20 @@
-source("main.R")
-source("helper.R")
+
+predict_class = function(filename = "class_predictions.csv")
+{
+model.control = trainControl(
+  method = "cv", # 'cv' for cross validation, 'adaptive_cv' drops unpromising models
+  number = 5, # number of folds in cross validation (or number of resampling iterations)
+  #repeats = 5, # number of repeats for repeated cross validation
+  search = "grid", # or grid for a grid search
+  classProbs = TRUE,
+  summaryFunction = twoClassSummary,
+  sampling = "smote", # This resolves class imbalances. 
+  # Possible values are "none", "down", "up", "smote", or "rose". The latter two values require the DMwR and ROSE packages, respectively.
+  allowParallel = TRUE, # Enable parallelization if available
+  returnData = FALSE) # The training data will not be included in the output training object
 
 # Train models
-xgb_parms_cphsv <- expand.grid(nrounds = c(800),
+xgb_parms_cphsv = expand.grid(nrounds = 800,
                            max_depth = 4,
                            eta = 0.01,
                            gamma = 0,
@@ -10,14 +22,14 @@ xgb_parms_cphsv <- expand.grid(nrounds = c(800),
                            min_child_weight = 1,
                            subsample = 0.8)
 
-xgb_mdl_cphsv <- train(return_customer~.,
+xgb_mdl_cphsv = train(return_customer~.,
                         data = known,
                         method = "xgbTree",
                         tuneGrid = xgb_parms_cphsv,
                         metric = "ROC",
                         trControl = model.control)
 
-xgb_woe_parms_cphsv <- expand.grid(nrounds = 200,
+xgb_woe_parms_cphsv = expand.grid(nrounds = 200,
                                max_depth = 4,
                                eta = 0.05,
                                gamma = 0,
@@ -25,7 +37,7 @@ xgb_woe_parms_cphsv <- expand.grid(nrounds = 200,
                                min_child_weight = 1,
                                subsample = 0.8)
 
-xgb_woe_mdl_cphsv <- train(return_customer~.,
+xgb_woe_mdl_cphsv = train(return_customer~.,
                        data = known_woe,
                        method = "xgbTree",
                        tuneGrid = xgb_woe_parms_cphsv,
@@ -35,21 +47,27 @@ xgb_woe_mdl_cphsv <- train(return_customer~.,
 lr_woe_mdl_cphsv = glm(return_customer~., data = known_woe, family = binomial(link = "logit"))
 rf_woe_mdl_cphsv = randomForest(return_customer~., data = known_woe, mtry = 8)
 
-
+neuralnet = nnet(return_customer ~ ., data = known_norm, size = 12, decay = 10, maxit = 1000)
 
 # predictions
 xgb_pred_cphsv = predict(xgb_mdl_cphsv, newdata = class, type = "prob")[,2]
 xgb_woe_pred_cphsv = predict(xgb_woe_mdl_cphsv, newdata = class_woe, type = "prob")[,2]
 lr_woe_pred_cphsv = predict(lr_woe_mdl_cphsv, newdata = class_woe, type = "response")
 rf_woe_pred_cphsv = predict(rf_woe_mdl_cphsv, newdata = class_woe, type = "prob")[,2]
-
+prediction_neuralnet = as.numeric(predict(neuralnet, newdata = class_norm, type = "raw"))
 
 # create data frame with all the predictions
-df_predictions_class <- data.frame(xgb_pred_cphsv, xgb_woe_pred_cphsv, lr_woe_pred_cphsv, rf_woe_pred_cphsv)
+df_predictions_class = data.frame(xgb = xgb_pred_cphsv, 
+                                  xgb_woe  = xgb_woe_pred_cphsv, 
+                                  logistic = lr_woe_pred_cphsv, 
+                                  random_forest = rf_woe_pred_cphsv,
+                                  neuralnet = prediction_neuralnet)
 
 # save it to csv file
-write.csv(x = df_predictions_class, file = "predictions_class.csv")
+print(paste("Writing to"), filename)
+write.csv(df_predictions_class, file = filename, row.names = row.names(class))
 
 # check correlations between the models
-cor(df_predictions_class)
 corrplot(cor(df_predictions_class))
+return(df_predictions_class)
+}
